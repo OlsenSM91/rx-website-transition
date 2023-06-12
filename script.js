d5189559e7250716a7db4bf7acb28b304cbda91e
OlsenSM91 <130707762+OlsenSM91@users.noreply.github.com>
2023-06-11 17:48:35 -0700

    Update script.js
    Add delay to the modal pop up

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+window.onload = function() {
+    // Get the modal
+    var modal = document.getElementById("myModal");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the span (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+    // When the user clicks on the span (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+
+    // Function to open the modal
+    function openModal() {
+      modal.style.display = "block";
+    }
+
+    // Call the function to open the modal with a 5 second delay
+    setTimeout(openModal, 5000);
 }
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-// Function to open the modal
-function openModal() {
-  modal.style.display = "block";
-}
-
-// Call the function to open the modal
-openModal();
