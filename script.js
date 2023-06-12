62efac3948bcf5698ddf9197a49bf0128390dc0e
OlsenSM91 <130707762+OlsenSM91@users.noreply.github.com>
2023-06-11 18:00:38 -0700

    Update script.js
    removed window.onclicked

--- script.js
+++ script.js
@@ -10,13 +10,6 @@ window.onload = function() {
       modal.style.display = "none";
     }
 
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
-
     // Function to open the modal
     function openModal() {
       modal.style.display = "block";
